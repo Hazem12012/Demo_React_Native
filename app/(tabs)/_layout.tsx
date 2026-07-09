25b0a504f923e0e1127a39d933eb9bdf87429f31
Hazem12012 <hazemmahmoud12012@gmail.com>
2026-07-10 02:37:30 +0300

    - Impelement the home bage

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -22,6 +22,7 @@ const TabLayout = () => {
     <Tabs
       screenOptions={{
         headerShown: false,
+
         tabBarShowLabel: false,
         tabBarStyle: {
           position: "absolute",
