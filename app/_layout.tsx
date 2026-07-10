2b26cf692b0d8c48ab7e5e1037d64c6b08b32c42
Hazem12012 <hazemmahmoud12012@gmail.com>
2026-07-10 21:15:37 +0300

    solve android issues

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,18 +1,17 @@
+import "@/global.css";
+import { ClerkProvider } from "@clerk/expo";
+import { tokenCache } from "@clerk/expo/token-cache";
 import { useFonts } from "expo-font";
 import { SplashScreen, Stack } from "expo-router";
 import { useEffect } from "react";
-import { ClerkProvider } from "@clerk/expo";
-import { tokenCache } from "@clerk/expo/token-cache";
-import "@/global.css";
 
 const publishableKey = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY!;
-
 
 if (!publishableKey) {
   throw new Error("Add your Clerk Publishable Key to the .env file");
 }
 
-SplashScreen.preventAutoHideAsync()
+SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
   const [fontsLoaded] = useFonts({
@@ -30,7 +29,7 @@ export default function RootLayout() {
   }, [fontsLoaded]);
 
   if (!fontsLoaded) return null;
-  
+
   return (
     <ClerkProvider publishableKey={publishableKey} tokenCache={tokenCache}>
       <Stack screenOptions={{ headerShown: false }} />
